0e7ee0217d9f52926ab78e9db3f918e77f0b6a25
hdavidpb <68169674+hdavidpb@users.noreply.github.com>
2024-12-18 21:47:56 -0500

    Update ProductsContainer.tsx

--- src/components/ProductsContainer.tsx
+++ src/components/ProductsContainer.tsx
@@ -1,20 +1,22 @@
-"use client";
-import { useContext } from "react";
+'use client'
+import { useContext, useEffect } from "react";
 import { ProductCard } from ".";
-import * as SC from "../styled-components/styles";
+import * as SC from "../styled-components/styles"
 import { stepsContext } from "@/context/StepsProvider";
 import useValidateSelectedProducts from "@/hooks/useValidateSelectedProducts";
+import { getProductsData } from "@/context/stepReducer/actions";
 import { ModalProduct } from "./ModalProduct";
 
-export const ProductsContainer = ({ products }: { products: any[] }) => {
-  const { state } = useContext(stepsContext);
+export const ProductsContainer = ({products}:{products:any[]}) => {
+  const { state, dispatch } = useContext(stepsContext);
   const { stepOneProducts } = state;
-
   useValidateSelectedProducts();
 
-
-
-
+useEffect(() => {
+  if(stepOneProducts.products.length === 0){
+    dispatch(getProductsData(products));
+  }
+}, [stepOneProducts.products]);
 
   return (
     <>
@@ -26,4 +28,4 @@ export const ProductsContainer = ({ products }: { products: any[] }) => {
       </SC.CardsContainer>
     </>
   );
-};
+}
